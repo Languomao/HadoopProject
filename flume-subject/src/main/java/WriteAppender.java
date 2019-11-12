bb04af40448db3bca945dc37eacadba96ce5fb3f
LanKorment <596268410@qq.com>
2019-11-12 14:22:12 +0800

    Commit by Lan

---
+++ flume-subject/src/main/java/WriteAppender.java
@@ -0,0 +1,32 @@
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+
+/**
+ * Created by Languomao on 2018/7/13.
+ */
+public class WriteAppender extends AppenderSkeleton {
+    private String account ;
+
+    @Override
+    protected void append(LoggingEvent event) {
+        System.out.println("Hello, " + account + " : "+ event.getMessage());
+    }
+
+    public void close() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public boolean requiresLayout() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+}
---
+++ flume-subject/src/main/java/WriteLog.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+import org.apache.log4j.Logger;
+
+/**
+ * Created by Languomao on 2018/7/12.
+ */
+public class WriteLog {
+    protected static final Logger logger = Logger.getLogger(WriteLog.class);
+
+    public static void main(String[] args) throws Exception {
+        while (true) {
+            logger.info("Hello,languomao:"+ String.valueOf(new Date().getTime()));
+            Thread.sleep(5000);
+        }
+    }
+}
